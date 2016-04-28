55f8168de289e6829e7de5990df2850e310ae9ac
zhulinpinyu <mulixiang199016@gmail.com>
2016-04-28 16:51:02 +0800

    Redux first try

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React,{Component} from 'react'
+import BookList from '../containers/book-list'
 
 export default class App extends Component {
     render(){
         return (
-            <div>Hi Book List</div>
+            <div>
+                <BookList />
+            </div>
         )
     }
 }
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: "React in Action"},
+        {title: "Crazy Openlayers3"},
+        {title: "The Dark Joy"},
+        {title: "Hery's Dog"}
+    ]
+}
